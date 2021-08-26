0747f09345303953afb641c9ec787f4e91aa420b
Mayra Pinot <mayra.pinot@detektor.com.hn>
2021-08-26 03:09:44 -0600

    Ägregando funcionalidad nivel de bateria

--- src/components/battery_component.js
+++ src/components/battery_component.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as Battery from 'expo-battery';
 import { Ionicons } from '@expo/vector-icons';
 import { StyleSheet, Text, View} from 'react-native';
 import BootstrapStyleSheet from 'react-native-bootstrap-styles';
@@ -7,11 +8,37 @@ const bootstrapStyleSheet = new BootstrapStyleSheet();
 const { s, c } = bootstrapStyleSheet;
 
 export default class BatteryComponent extends React.Component {
+  state = {
+    batteryLevel: null,
+  };
+
+  componentDidMount() {
+    this._subscribe();
+  }
+
+  componentWillUnmount() {
+    this._unsubscribe();
+  }
+
+  async _subscribe() {
+    const batteryLevel = await Battery.getBatteryLevelAsync() * 100;
+    this.setState({ batteryLevel: Math.trunc(batteryLevel)});
+   
+    this._subscription = Battery.addBatteryLevelListener(({ batteryLevel }) => {
+      this.setState({ batteryLevel });
+    });
+  }
+
+  _unsubscribe() {
+    this._subscription && this._subscription.remove();
+    this._subscription = null;
+  }
+
   render(){
       return(
           <View style={[styles.box, {flexDirection: "row" }]}>
             <Ionicons name="md-battery-half" size={23} color="#80deea" />
-            <Text style={[s.btnPrimaryText, {marginVertical : 4, marginLeft:10}]}>Battery level: 12%</Text>
+            <Text style={[s.btnPrimaryText, {marginVertical : 4, marginLeft:10}]}>Battery level: {this.state.batteryLevel}%</Text>
           </View>
       );
   }
