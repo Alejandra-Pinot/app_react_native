cd3d4762802676bd05670a44d130a1221fd81eb0
Mayra Pinot <mayra.pinot@detektor.com.hn>
2021-08-27 22:44:38 -0600

    Configuración e implementación de notificaciones, recibe un push notification al salir del app y al presionar lo lleva de vuelta a la aplicación

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
-import React, { useEffect } from 'react';
+import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications';
+import React, { useState, useEffect, useRef } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { AppState } from "react-native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -8,22 +10,103 @@ import TakePicturePage from './src/views/takePicture_page';
 import DisplayPicturePage from './src/views/displayPicture_page';
 
 const Stack = createNativeStackNavigator();
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 function App() {
   global.isPickerPicture=false;
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  // useEffect(() => {
+  //   const subscription = AppState.addEventListener("change", nextAppState => {
+  //     console.log(global.isPickerPicture);
+  //     if (nextAppState !== "active" && !global.isPickerPicture) {
+  //       console.log("MANDAR NOTIFICACIÓN");
+  //     }
+  //   });
+
+  //   return () => {
+  //     subscription.remove();
+  //   };
+  // }, []);
 
   useEffect(() => {
-    const subscription = AppState.addEventListener("change", nextAppState => {
-      console.log(global.isPickerPicture);
-      if (nextAppState !== "active" && !global.isPickerPicture) {
-        console.log("MANDAR NOTIFICACIÓN");
-      }
+    //Config notifications
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
     });
 
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    //Status app
+    const subscription = AppState.addEventListener("change", nextAppState => {
+        console.log(global.isPickerPicture);
+        if (nextAppState !== "active" && !global.isPickerPicture) {
+          console.log("MANDAR NOTIFICACIÓN");
+          schedulePushNotification();
+        }
+      });
+
     return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
       subscription.remove();
     };
   }, []);
+
+  async function schedulePushNotification() {
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "APP Notification 🔔",
+        body: 'Press to return to the app',
+        data: { data: 'goes here' },
+      },
+      trigger: { seconds: 2 },
+    });
+  }
+
+  async function registerForPushNotificationsAsync() {
+    let token;
+    if (Constants.isDevice) {
+      const { status: existingStatus } = await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== 'granted') {
+        alert('Failed to get push token for push notification!');
+        return;
+      }
+      token = (await Notifications.getExpoPushTokenAsync()).data;
+      console.log(token);
+    } else {
+      alert('Must use physical device for Push Notifications');
+    }
+  
+    if (Platform.OS === 'android') {
+      Notifications.setNotificationChannelAsync('default', {
+        name: 'default',
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: '#FF231F7C',
+      });
+    }
+  
+    return token;
+  }
 
   return (
     <NavigationContainer>
